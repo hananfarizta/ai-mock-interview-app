b9f967e8ba6aac20ef717f8a584c4f72ef2f8ea2
hananfarizta <hananfariztaa@gmail.com>
2025-09-19 22:08:33 +0700

    feat/add authentication

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Header() {
@@ -11,7 +12,9 @@ function Header() {
         <Image src={"/logo.svg"} alt="Logo" width={40} height={40} />
         <h1 className="text-base font-bold md:text-2xl">AI Mock Interview</h1>
       </div>
-      <Button>Get Started</Button>
+      <Link href={"/dashboard"}>
+        <Button size={"lg"}>Get Started</Button>
+      </Link>
     </nav>
   );
 }
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { motion } from "motion/react";
+import Link from "next/link";
 import React from "react";
 
 function Hero() {
@@ -66,7 +67,9 @@ function Hero() {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <Button size={"lg"}>Explore Now</Button>
+          <Link href={"/dashboard"}>
+            <Button size={"lg"}>Explore Now</Button>
+          </Link>
           <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
             Contact Support
           </button>
